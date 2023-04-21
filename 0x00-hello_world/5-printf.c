906846d781c184ae4d07457379c8863751d6ac26
wanjirumwai <wanjiru_mwai@gmail.com>
2023-04-21 04:44:58 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- * main - A programe that uses the function printf
+ * Description: main - Print a c program using printf
+ *
  * Return: 0
- */
+*/
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n")
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
