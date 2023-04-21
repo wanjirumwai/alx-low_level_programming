977662af11b1d05b8b6969b0699feb658d682c87
wanjirumwai <wanjiru_mwai@gmail.com>
2023-04-21 03:07:32 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A programe that prints a line with puts funtion
+ * Return: 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
