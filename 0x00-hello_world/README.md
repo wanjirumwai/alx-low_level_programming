99a6b2bc95008cb1b48d7b29052e8fac13f19c6e
wanjirumwai <wanjiru_mwai@gmail.com>
2023-05-03 00:39:37 -0700

    Restore files

---
+++ 0x00-hello_world/README.md
@@ -0,0 +1,1 @@
+the initial processes in C
--- 0x04-more_functions_nested_loops/2-main.c
+++ 0x04-more_functions_nested_loops/2-main.c
@@ -8,11 +8,7 @@
  */
 int main(void)
 {
-int n;
-
-n = 402;
-printf("n=%d\n", n);
-reset_to_98(&n);
-printf("n=%d\n", n);
-return (0);
+	printf("%d\n", mul(98, 1024));
+	printf("%d\n", mul(-402, 4096));
+	return (0);
 }
