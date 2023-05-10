125d3756bc3a1e559a481e0fa8d5be8f9cb14c96
wanjirumwai <wanjiru_mwai@gmail.com>
2023-05-10 11:41:34 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,9 +1,11 @@
 #include "main.h"
+
 /**
- * _puts_recursion - function like puts();
- * @s: input
- * Return: Always 0
+ *_puts_recursion - formula for funtion like puts();
+ *@s: input
+ *Return: Always 0
  */
+
 void _puts_recursion(char *s)
 {
 	if (*s)
@@ -11,7 +13,6 @@ void _puts_recursion(char *s)
 		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
-
 	else
-	_putchar('\n');
+		_putchar('\n');
 }
