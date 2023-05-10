ca868a6ec4a571e24c4516f71323886de72026b5
wanjirumwai <wanjiru_mwai@gmail.com>
2023-05-10 05:16:54 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,14 +2,14 @@
 /**
  * _puts_recursion - function like puts();
  * @s: input
- * Return: 0
+ * Return: Always 0
  */
 void _puts_recursion(char *s)
 {
 	if (*s)
 	{
-	_putchar(*s);
-	_puts_recursion(s + 1);
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
 
 	else
