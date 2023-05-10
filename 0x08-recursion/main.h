d63ef95416f165334edec920624c7689f97de5cc
wanjirumwai <wanjiru_mwai@gmail.com>
2023-05-10 04:51:37 -0700

    main.h

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,5 +1,6 @@
 #define MAIN_H
 
+int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
@@ -8,4 +9,3 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
-int _putchar(char c);
