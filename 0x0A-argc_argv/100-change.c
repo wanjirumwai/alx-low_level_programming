2f629afcc96df6b4d024be565b1e094dc2914e6e
wanjirumwai <wanjiru_mwai@gmail.com>
2023-05-12 12:25:56 -0700

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -3,7 +3,8 @@
 #include "main.h"
 
 /**
- * main - formula that prints the minimum number of coins to make change for an amount of money
+ * main - formula that prints the minimum number of 
+ * coins to make change for an amount of money
  * @argc: number of arguments
  * @argv: array of arguments
  *
