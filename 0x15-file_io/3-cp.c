b8895e1d7a85a35351d64935e71b8774b807ca0a
wanjirumwai <wanjiru_mwai@gmail.com>
2023-06-12 04:37:13 -0700

    a program that copies the content of a file to another file

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,105 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+char *create_buffer(char *file);
+void close_file(int fd);
+
+/**
+ * create_buffer -This function will allocates 1024 bytes for a buffer.
+ * @file:Points to the name of the file to buffer
+ *
+ * Return: To the newly allocated buffer.
+ */
+char *create_buffer(char *file)
+{
+	char *buffer;
+
+	buffer = malloc(sizeof(char) * 1024);
+
+	if (buffer == NULL)
+	{
+		dprintf(STDERR_FILENO,
+			"Error: Can't write to %s\n", file);
+		exit(99);
+	}
+
+	return (buffer);
+}
+
+/**
+ * main -This function will cop the contents
+ * of a file to another.
+ * @argc: Points to nunber of arguments fed to the program.
+ * @argv: Points to an array commands fed to the arguments.
+ *
+ * Return: 0 upon success.
+ *
+ * Description:Incorrect arguement - exit code 97.
+ * If file(from) does not exist or can't be read - exit code 98.
+ * If file(to) wasnt created or written into - exit code 99.
+ * If file_to or file_from cant be shut down - exit code 100.
+ */
+int main(int argc, char *argv[])
+{
+	int from, to, r, w;
+	char *buffer;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	buffer = create_buffer(argv[2]);
+	from = open(argv[1], O_RDONLY);
+	r = read(from, buffer, 1024);
+	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+	do {
+		if (from == -1 || r == -1)
+		{
+			dprintf(STDERR_FILENO,
+				"Error: Can't read from file %s\n", argv[1]);
+			free(buffer);
+			exit(98);
+		}
+
+		w = write(to, buffer, r);
+		if (to == -1 || w == -1)
+		{
+			dprintf(STDERR_FILENO,
+				"Error: Can't write to %s\n", argv[2]);
+			free(buffer);
+			exit(99);
+		}
+
+		r = read(from, buffer, 1024);
+		to = open(argv[2], O_WRONLY | O_APPEND);
+
+	} while (r > 0);
+
+	free(buffer);
+	close_file(from);
+	close_file(to);
+
+	return (0);
+}
+
+/**
+ * close_file -This function will Closes file descriptors.
+ * @fd:Points to the Descriptor.
+ */
+void close_file(int fd)
+{
+
+	int c;
+
+
+	c = close(fd);
+if (c == -1)
+{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
